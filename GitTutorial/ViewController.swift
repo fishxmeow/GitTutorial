664315154d60784dc868526fa882038bb6239a68
fatimaMadan <202001312@student.polytechnic.bh>
2023-11-15 13:07:58 +0300

    done

--- GitTutorial/ViewController.swift
+++ GitTutorial/ViewController.swift
@@ -9,25 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var reversedLabel: UILabel!
     let message = "Hello Git!"
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(reverse(text: "stressed"))
-        
         print(message)
+        
+        let reversed = reverse(text: "Stressed")
+        print(reversed)
+        
+        reversedLabel.text = reversed
         // Do any additional setup after loading the view.
     }
     
-    func reverse(text: String) -> String {
-        return String(text.reversed())
-    }
 
-
+    
         override func didReceiveMemoryWarning() {
             super.didReceiveMemoryWarning()
             // Dispose of any resources that can be recreated.
         }
+    
+    func reverse(text: String) -> String {
+        return String(text.reversed())
+    }
 }
 
